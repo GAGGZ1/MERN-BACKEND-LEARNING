e645c825d3d8a391ac22553223e38c7fe5fdc1c4
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-05-20 23:27:18 +0530

    add pre-save middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,69 +1,92 @@
-const User=require("../models/User");
-const bcrypt=requie("bcryptjs");
-const jwt=require("jsonwebtoken")
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const registerUser=async(req,res)=>{
-  try{
-    const {name,email,password}=req.body;
-    const existingUser=await User.findOne({email});
-    if(existingUser){
+// Register User
+const registerUser = async (req, res) => {
+
+  try {
+
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
       return res.status(400).json({
-        message:"User already exists"
+        message: "User already exists"
       });
     }
-    const hashedPassword=await bcrypt.hash(password,10);
-    //create User
-    const user=await User.create({
-      name,email,password:hashedPassword
+
+    // Password hashing handled by pre-save middleware
+    const user = await User.create({
+      name,
+      email,
+      password
     });
+
     res.status(201).json({
-      message:"User registered"
+      message: "User registered"
     });
-  }
-  catch(error)
-  {
+
+  } catch (error) {
+
     res.status(500).json({
-      message:error.message
-    })
+      message: error.message
+    });
+
   }
-}
+};
 
-const loginUser=async (req,res)=>{
-  try{
-    const {email,password}=req.body;
-    const user=await User.findOne({email});
+// Login User
+const loginUser = async (req, res) => {
 
-    if(!user){
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
       return res.status(401).json({
-        message:"Invalid credentials"
+        message: "Invalid credentials"
       });
     }
 
-    //compare passwords
+    // Compare passwords
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
 
-    const isMatch=await bcrypt.compare(password,hashedPassword);
-
-    if(!isMatch){
+    if (!isMatch) {
       return res.status(401).json({
-        message:"Invalid credentials"
-      })
+        message: "Invalid credentials"
+      });
     }
 
-    //generate JWT token
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        id: user._id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d"
+      }
+    );
 
-    const token=jwt.sign({
-      id:user._id
-    },
-  process.env.JWT_SECRET,
-{
-  expiresIn:"1d"
-});
+    res.json({ token });
 
-res.json({token});
-  }
-  catch(error){
+  } catch (error) {
+
     res.status(500).json({
-      message:error.message
-    })
+      message: error.message
+    });
+
   }
-}
+};
+
+module.exports = {
+  registerUser,
+  loginUser
+};
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose =require("moongose");
+const bcrypt=require("bcryptjs");
 const userSchema=new mongoose.Schema({
   name:{
     type:String,
@@ -16,5 +17,12 @@ const userSchema=new mongoose.Schema({
 },{
   timestamps:true
 });
+userSchema.pre("save",async function(next){
+  if(!this.isModified("password")){
+    return next();
+  }
+  this.password=await bcrypt.hash(this.password,10);
 
+  next();
+})
 module.exports=mongoose.model("User",userSchema);
